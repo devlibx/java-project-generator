3927029c1e01df9b416fe1fd15305c32efa1ac20
Harish Bohara <you@example.com>
2021-06-17 14:33:30 +0530

    using easy 0.0.54

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
-mvn -B clean install -Dtag=2.0.0 -Dmaven.test.skip=true release:prepare release:perform -P release
+mvn -B clean install -Dtag=2.0.2 -Dmaven.test.skip=true release:prepare release:perform -P release
 
 # Rollback
 # mvn release:rollback
-# git tag -d 2.0.0
-# git push --delete origin 2.0.0
+# git tag -d 2.0.2
+# git push --delete origin 2.0.2
